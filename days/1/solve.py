f4a44f699644d5b9e549ed7e3898963c7e53478e
Joe Webster <joe@joewebster.net>
2024-12-02 16:56:35 +0000

    🎅🏻

---
+++ days/1/solve.py
@@ -0,0 +1,9 @@
+def solve(data_filename, part2=False):
+	pass
+
+if __name__ == "__main__":
+	print("Part 1 Test: ", solve('test_data.txt'))
+	print("Part 1: ", solve('data.txt'))
+	
+	print("Part 2 Test:", solve('test_data.txt', part2=True))
+	print("Part 2:", solve('data.txt', part2=True)) 
