10d29ac04e6f2c2a84d8a7176a4d42124aba14c4
Joe Webster <joe@joewebster.net>
2024-12-02 18:18:28 +0000

    re and zip on day 1? :oof:

--- days/1/solve.py
+++ days/1/solve.py
@@ -1,9 +1,35 @@
+import re
+
+def read_data(filename):
+	with open(filename) as fp:
+		return zip(*(re.split(r'\s+', l.strip()) for l in fp.readlines()))
+
 def solve(data_filename, part2=False):
-	pass
+	list1, list2 = read_data(data_filename)
+
+	if not part2:
+		return calc_dist(list1, list2)
+	else:
+		return calc_sim(list1, list2)
+
+def calc_dist(list1, list2):
+	dist = 0
+	for a, b in zip(sorted(list1), sorted(list2)):
+		dist += abs(int(a)-int(b))
+
+	return dist
+
+def calc_sim(list1, list2):
+	sim = 0
+	for a in sorted(list1):
+		occ = sum([1 for b in list2 if b==a])
+		sim += int(a)*occ
+
+	return sim
 
 if __name__ == "__main__":
-	print("Part 1 Test: ", solve('test_data.txt'))
-	print("Part 1: ", solve('data.txt'))
+	print("Part 1 Test: ", solve('days/1/test_data.txt'))
+	print("Part 1: ", solve('days/1/data.txt'))
 	
-	print("Part 2 Test:", solve('test_data.txt', part2=True))
-	print("Part 2:", solve('data.txt', part2=True)) 
+	print("Part 2 Test:", solve('days/1/test_data.txt', part2=True))
+	print("Part 2:", solve('days/1/data.txt', part2=True)) 
